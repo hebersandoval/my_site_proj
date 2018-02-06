2cd2118155c126f740b7f7e6b4e8daae7c0a3381
Heber Sandoval <he3ers@gmail.com>
2018-02-06 14:59:04 -0500

    Add a PostModelAdmin class for postd admin site.

--- posts/admin.py
+++ posts/admin.py
@@ -3,4 +3,13 @@ from django.contrib import admin
 # Register your models here.
 from .models import Post
 
-admin.site.register(Post)
+class PostModelAdmin(admin.ModelAdmin):
+    # '__unicode__' refers to the 'title' attribute and can be substituted
+    list_display = ['__unicode__', 'timestamp', 'updated']
+    list_display_links = ['__unicode__']
+    list_filter = ['timestamp', 'updated']
+    search_fields = ['title', 'content']
+    class Meta:
+        model = Post
+
+admin.site.register(Post,PostModelAdmin)
