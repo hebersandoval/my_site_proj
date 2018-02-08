357db810501e405e75b48462401c234395620d67
Heber Sandoval <he3ers@gmail.com>
2018-02-08 14:40:33 -0500

    Setup up static files, js and css to serve from a separate local emulated server, it's just a directory outside the project's directory but will help when pushing to S3 bucket.Yeya!

--- my_site_proj/settings.py
+++ my_site_proj/settings.py
@@ -120,3 +120,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# ~/proj/sector_micro/django/my_site_proj/static
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    # '/var/www/static',
+]
+
+# heber/proj/sector_micro/django/static_cdn
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static_cdn')
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'media_cdn')
