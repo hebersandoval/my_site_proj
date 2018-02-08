7a1b0c29311aa535fa7c05cee1c1210688f0c35e
Heber Sandoval <he3ers@gmail.com>
2018-02-08 06:54:43 -0500

    Add templates directroy for posts and adjusted ulrs.py and settings.py to find path for templates.

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,19 @@
 from django.conf.urls import url
 from django.contrib import admin
 
+from .views import (
+    post_list,
+    post_create,
+    post_detail,
+    post_update,
+    post_delete,
+)
+
 urlpatterns = [
     # url(r'^posts/$', '<app_name>.views.<function_name>'),
-    url(r'^$', 'posts.views.post_home'),
+    url(r'^$', post_list),
+    url(r'^create/$', post_create),
+    url(r'^(?P<id>\d+)/$', post_detail),
+    url(r'^update/$', post_update),
+    url(r'^delete/$', post_delete)
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
 # Create your views here.
+from .models import Post
+
 # function base views
-def post_home(request):
-    return HttpResponse('<h1>Hello</h1>')
-    # render()
+def post_create(request):
+    return HttpResponse('<h1>Create</h1>')
+
+def post_detail(request, id=None):
+    post = get_object_or_404(Post, id=id)
+    context = {
+        'title': 'Detail',
+        'post': post,
+    }
+    return render(request, 'posts/detail.html', context)
+
+def post_list(request):
+    posts = Post.objects.all()
+    context = {
+        'title': 'List',
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context)
+
+def post_update(request):
+    return HttpResponse('<h1>Update</h1>')
+
+def post_delete(request):
+    return HttpResponse('<h1>Delete</h1>')  
