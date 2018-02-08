0c9f38481f2ef81b389688a566076c7d7ef63a69
Heber Sandoval <he3ers@gmail.com>
2018-02-08 09:15:53 -0500

    Add url links and get_absolute_ulr().

--- posts/models.py
+++ posts/models.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.core.urlresolvers import reverse
 
 # Create your models here.
 class Post(models.Model):
@@ -16,3 +17,7 @@ class Post(models.Model):
     # used in python 2
     def __unicode__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('posts:detail', kwargs={'id': self.id})
+        # return '/posts/%s/' %(self.id)
